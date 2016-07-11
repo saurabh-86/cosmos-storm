92ddd40ebcda46fb02837147852ba1ac6d39240a
Saurabh Agrawal <saurabh.agrawal@flipkart.com>
2016-07-11 10:10:12 +0530

    remove init script

--- pkg/create_deb.sh
+++ pkg/create_deb.sh
@@ -9,20 +9,20 @@ rm -rf $PACKAGE_ROOT
 
 echo "Creating temp packaging directory ${PACKAGE_ROOT} ..."
 mkdir -p $PACKAGE_ROOT
-#mkdir -p $PACKAGE_ROOT/DEBIAN
+mkdir -p $PACKAGE_ROOT/DEBIAN
 mkdir -p $PACKAGE_ROOT/var/lib/$PACKAGE
 mkdir -p $PACKAGE_ROOT/etc/$PACKAGE
-mkdir -p $PACKAGE_ROOT/etc/init.d
+#mkdir -p $PACKAGE_ROOT/etc/init.d
 mkdir -p $PACKAGE_ROOT/etc/cron.d
 mkdir -p $PACKAGE_ROOT/etc/confd/templates
 mkdir -p $PACKAGE_ROOT/etc/confd/conf.d/
 
 echo "Copying debian files to ${PACKAGE_ROOT} ..."
-#cp pkg/deb/$PACKAGE.control $PACKAGE_ROOT/DEBIAN/control
-#cp pkg/deb/$PACKAGE.postinst $PACKAGE_ROOT/DEBIAN/postinst
-#cp pkg/deb/$PACKAGE.postrm $PACKAGE_ROOT/DEBIAN/postrm
-#cp pkg/deb/$PACKAGE.preinst $PACKAGE_ROOT/DEBIAN/preinst
-#cp pkg/deb/$PACKAGE.prerm $PACKAGE_ROOT/DEBIAN/prerm
+cp pkg/deb/$PACKAGE.control $PACKAGE_ROOT/DEBIAN/control
+cp pkg/deb/$PACKAGE.postinst $PACKAGE_ROOT/DEBIAN/postinst
+cp pkg/deb/$PACKAGE.postrm $PACKAGE_ROOT/DEBIAN/postrm
+cp pkg/deb/$PACKAGE.preinst $PACKAGE_ROOT/DEBIAN/preinst
+cp pkg/deb/$PACKAGE.prerm $PACKAGE_ROOT/DEBIAN/prerm
 #cp pkg/deb/$PACKAGE-init $PACKAGE_ROOT/etc/init.d/$PACKAGE
 
 echo "Updating version in control file ..."
